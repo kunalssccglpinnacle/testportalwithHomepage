8d76ac01767017cff4fb65d5fd3c20c73eff7072
Kunalsharmassccglpinnacle <kunalsharma@ssccglpinnacle.com>
2024-07-16 16:26:40 +0530

    somechanges1

--- app/src/main/java/com/ssccgl/pinnacle/testportal/UI Layer/MainScreen.kt
+++ app/src/main/java/com/ssccgl/pinnacle/testportal/UI Layer/MainScreen.kt
@@ -47,10 +47,9 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.ssccgl.pinnacle.testportal.viewmodel.HomeViewModel
-import com.ssccgl.pinnacle.testportal.R
 import kotlinx.coroutines.launch
 
 @Composable
@@ -58,17 +57,19 @@ fun MainScreen(homeViewModel: HomeViewModel) {
     val scaffoldState = rememberScaffoldState()
     val navController = rememberNavController()
     val coroutineScope = rememberCoroutineScope()
+    val currentBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = currentBackStackEntry.value?.destination?.route
 
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = {
-            if (navController.currentDestination?.route == "dashboard") {
+            if (currentRoute == "dashboard") {
                 TopAppBarWithDrawerButton(scaffoldState)
             }
         },
         drawerContent = { AppDrawer(navController, scaffoldState) },
         bottomBar = {
-            if (navController.currentDestination?.route == "dashboard") {
+            if (currentRoute == "dashboard") {
                 BottomNavigationBar(navController)
             }
         },
